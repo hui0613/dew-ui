f754a18ea3f40a91f71082964255ca27cf8dd29a
hui0613 <2516772557@qq.com>
2022-12-03 14:41:26 +0000

    【Update】 generate .d.ts

--- packages/utils/vue/install.ts
+++ packages/utils/vue/install.ts
@@ -1,5 +1,7 @@
-import type { SFCWithInstall } from "./typescript"
+import type { SFCWithInstall } from './typescript'
 import type { App } from 'vue'
+// 这个地方需要引入 @vue/shard ， 在生成 .d.ts 声明文件时才不会报错
+import type { NOOP } from '@vue/shared'
 
 export const withInstall = <T, E extends Record<string, any>>(main: T, extra?: E) => {
   ;(main as SFCWithInstall<T>).install = (app: App): void => {
